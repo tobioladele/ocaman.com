31da0bfaba66ba58874bc47b438ae1c7e6ce70b2
tedlade <you@example.com>
2015-05-11 23:47:37 +0100

    first commit

---
+++ js/animations.js
@@ -0,0 +1,11 @@
+$("#filter").click(function(){
+	$(".mobileSearch").show();
+	$("#container").hide();
+});	
+
+
+$(".close").click(function(){
+	$(".mobileSearch").hide();
+	$("#container").show();
+});	
+
